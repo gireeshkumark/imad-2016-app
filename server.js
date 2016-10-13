df8c4eee65c5067f1e90cc0c4d4cd5ca11f62a35
gireeshkumark <gireeshthattarethu@gmail.com>
2016-10-13 13:44:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,res){
-    res.send("Article one requested and will send here");
+    res.sendFile(path.join(__dirname, 'ui', 'article.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
